b01296e63a3b6348137ede9061d507564296896b
anuin00 <123932733+anuin00@users.noreply.github.com>
2023-02-02 20:54:17 +0700

    Update config.js
    Signed-off-by: anuin00 <123932733+anuin00@users.noreply.github.com>

--- config.js
+++ config.js
@@ -37,10 +37,10 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['27634090203','']
-global.premium = ['27634090203','918188019676']
-global.botname = 'ZIM BOT INC'
-global.ownername = 'DRIPS'
+global.owner = ['6281939183545','']
+global.premium = ['6281939183545','6281381301310']
+global.botname = 'anu_'
+global.ownername = 'anunem_'
 global.group1 = 'https://chat.whatsapp.com/EFsb8RCXV4jLEFk4eAcA1A'
 global.group2 = 'https://chat.whatsapp.com/HSfcYU13g5C8GxINWwSWoA'
 global.packname = 'ZIM BOT INC'
